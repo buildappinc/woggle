f4d36d7e1873638722be53579282823790ee4f11
Kenneth <kyleabs20@gmail.com>
2020-08-18 12:08:19 -0700

    trying again

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -67,12 +67,13 @@ class AdminController extends Controller
        if ($request->hasfile('image')) {
             $image = $request->file('image');
             $file_name = time() . '.' . $image->getClientOriginalExtension();
-            $location = public_path('images/courses/'.$file_name);
-            Image::make($image)->resize(109, 80)->save($location);
+            $imagePath = $file_name->store('uploads', 'public');
+            // $location = public_path('images/courses/'.$file_name);
+            // Image::make($image)->resize(109, 80)->save($location);
             // $imagePath = request()->image->store('uploads', 'public');
 
-            // $Image = Image::make(public_path("storage/{$imagePath}"))->resize(109, 80);
-            // $Image->save(80);
+            $Image = Image::make(public_path("storage/{$imagePath}"))->resize(109, 80);
+            $Image->save(80);
        }
 
         Course::create([
