945d7a48073aac0af56cf9fcbb4d35687f5d7d42
Kenneth <kyleabs20@gmail.com>
2020-08-18 21:07:07 -0700

    #

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -57,26 +57,26 @@ class AdminController extends Controller
             'description' => 'required | max:1000'
         ]);
 
-        // if ($request->hasFile('image')){
-        //     $image = $request->file('image');
-        //     $file_name = time() . '.' . $image->getClientOriginalExtension();
-        //     $location = public_path('images/courses/'.$file_name);
-        //     Image::make($image)->resize(800,400)->save($location);
-        // } 
+        if ($request->hasFile('image')){
+            $image = $request->file('image');
+            $file_name = time() . '.' . $image->getClientOriginalExtension();
+            $location = public_path('images/courses/'.$file_name);
+            Image::make($image)->resize(109, 80)->save($location);
+        } 
 
-       if ($request->hasfile('image')) {
-            $image = request()->image;
-            $imagePath = $image->store('uploads', 'public');
+    //    if ($request->hasfile('image')) {
+    //         $image = request()->image;
+    //         $imagePath = $image->store('uploads', 'public');
 
-            $Image = Image::make(public_path("storage/{$imagePath}"))->resize(109, 80);
-            $Image->save(80);
-       }
+    //         $Image = Image::make(public_path("storage/{$imagePath}"))->resize(109, 80);
+    //         $Image->save(80);
+    //    }
 
         Course::create([
             'name' => $data['name'],
             'price' => $data['price'], 
             'introduction' => $data['introduction'], 
-            'image' => $imagePath, 
+            'image' =>  $file_name, 
             'description' => $data['description']
         ]);
 
