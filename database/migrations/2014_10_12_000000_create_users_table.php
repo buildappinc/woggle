3f5efbc428fd726ad5a210b380ad12a210939984
Kenneth <kyleabs20@gmail.com>
2020-08-21 10:17:43 -0700

    work

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('image');
+            $table->string('image')->default("");
             $table->rememberToken();
             $table->timestamps();
         });
