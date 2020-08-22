8a4611503fc7e10f3400981ba308b61f290af472
Kenneth <kyleabs20@gmail.com>
2020-08-21 18:05:21 -0700

    something to live for

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -13,25 +12,12 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request, AuthenticationException $exception)
+    protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
 
-            $guard = array_get($exception->guards(), 0);
-
-            switch ($guard) {
-                case 'admin':
-                    $login = 'admin.login';
-                    break;
-                
-                default:
-                    $login = 'login';
-                    break;
-            }
-    
-            return redirect()->guest(route($login));
-
-            // return route('admin.login');
+            dd($request->expectsJson());
+            return route('admin.login');
         }
     }
 }
