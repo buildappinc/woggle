fae58bbe97cd508a81eaecc63a44fb0b7ad211b7
Kenneth <kyleabs20@gmail.com>
2020-08-24 14:00:21 -0700

    fonts reload

--- resources/views/Study/show.blade.php
+++ resources/views/Study/show.blade.php
@@ -1,4 +1,18 @@
+<style>
+    .text_font{
+        font-family: 'Baloo Tamma 2', cursive;
+        font-family: 'Comfortaa', cursive;
+        font-family: 'Raleway', sans-serif;
+        font-family: 'Recursive', sans-serif;
+        font-family: 'Tajawal', sans-serif;
+    }
 
+    .font{
+        font-family: 'Baloo Tamma 2', cursive;
+        font-family: 'Raleway', sans-serif;
+        font-family: 'Recursive', sans-serif;
+    }
+</style>
 @extends('layouts.app')
 
 @section('title', 'course')
@@ -25,7 +39,7 @@
                                     <div class="mt-3">
                                         <a href="{{ route('payment.index', ['course'=>$course->id])}}" style="text-decoration: none; color: #000">
                                             <div style="background-color: #f7b728; height: 40px; width: 100%; border-radius: 3px;">
-                                                  <p style="text-align: center; padding-top: 6%; text-transform: uppercase">Enroll now</p> 
+                                                  <p style="font text-align: center; padding-top: 6%; text-transform: uppercase">Enroll now</p> 
                                             </div>
                                         </a>
                                         {{-- <form action="{{route('course.get', ['course'=>$course->id])}}" method="post">
@@ -36,10 +50,10 @@
                                 </div>
                                 <div class="col-span-3 ...">
                                     <div>
-                                        <h2>
+                                        <h2 class="font">
                                             {{$course->name}}
                                         </h2>
-                                        <div> <p style="line-height: 30px;" class="text-justify">{{$course->description}}</p></div>
+                                        <div> <p style="line-height: 30px;" class="tect_font text-justify">{{$course->description}}</p></div>
                                     </div>
                                 </div>
                             </div>
@@ -49,22 +63,22 @@
                         {{-- buttom right section --}}
                         <div class="shadow-md rounded-md p-4 animated fadeInUp">
                             <div class="flex justify-between">
-                                <div>Topics</div>
-                                <div>Practical exercise</div>
+                                <div class="font">Topics</div>
+                                <div class="font">Practical exercise</div>
                             </div>
 
                             <div class="mt-3">
                                 @foreach ($course->topics as $topic)
                                  <div>
-                                     <div class="pt-2 pl-3 text-base pb-2" style="background: linear-gradient(183.72deg, #666666 0%, #151522 100%); border-radius: 5px; color: #fff">
+                                     <div class="font pt-2 pl-3 text-base pb-2" style="background: linear-gradient(183.72deg, #666666 0%, #151522 100%); border-radius: 5px; color: #fff">
                                        Topic: {{$topic->name}}
                                      </div>
                                      <div class="mt-2">
                                      @foreach ($topic->sections as $section)
                                          <div class="p-1" style="background-color: #f7f7f7">
                                             <div class="flex justify-between">
-                                                <div class="pl-3">Section - {{$section->header}}</div>
-                                                <div class="pr-3">10 mins</div>
+                                                <div class="text_font pl-3">Section - {{$section->header}}</div>
+                                                <div class="font pr-3">10 mins</div>
                                             </div>
                                             <hr>
                                          </div>
@@ -80,15 +94,15 @@
             <div class="col-span-1 hidden sm:hidden md-block lg:block xl:block">
                 <div class="shadow-md rounded-md max-h-full animated fadeInRight">
                     <div>
-                        <div class="pl-4 pt-2 text-2xl">Course Author</div> 
+                        <div class="font pl-4 pt-2 text-2xl">Course Author</div> 
                         <hr>
                     </div>
                     <div class="pl-4 pb-4 ">
                         <div class="flex">
-                            <div>dad</div>
-                            <div class="mx-16">Sir John</div>
+                            <div class="font">dad</div>
+                            <div class="font mx-16">Sir John</div>
                         </div>
-                        <div class="pr-4 pt-3 text-justify">
+                        <div class="font pr-4 pt-3 text-justify">
                             Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto in magnam animi molestias, doloremque odio, soluta, aliquam placeat illo dignissimos quae ab tenetur perferendis fuga ipsa maiores commodi vitae dolores. consectetur adipisicing elit. Dolores explicabo blanditiis sunt placeat sapiente. Quos autem officia quae illo incidunt perspiciatis consectetur unde, eum commodi sunt nesciunt, reiciendis neque dolore!
                         </div>
                     </div>
