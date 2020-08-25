28ae0635d6db2811bf153b81c2584e01667885cb
Kenneth <kyleabs20@gmail.com>
2020-08-24 20:10:27 -0700

    admin something

--- resources/views/Admin/admin.blade.php
+++ resources/views/Admin/admin.blade.php
@@ -116,7 +116,7 @@
                     </div>
                     @foreach ($courses as $item)
                         <div class="w-full p-4 flex flex-col justify-around col-gap-6 ">
-                            <a class="text-black">
+                            <a class="text-black" href="/admin/courses/{{$item->id}}/edit">
                                 <div class="w-11/12 h-16 bg-gray-200 rounded-lg flex flex-row justify-between px-4 p-4 hover:shadow-lg">
                                     <div>{{$item->name}}</div>
                                     <div>{{$item->price}}</div>
