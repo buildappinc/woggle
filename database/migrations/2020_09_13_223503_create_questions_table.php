aff41b215022c838bf4c269082560099e6d21ae5
Kenneth <kyleabs20@gmail.com>
2020-09-14 08:30:31 -0700

    working on the database

--- database/migrations/2020_09_13_223503_create_questions_table.php
+++ database/migrations/2020_09_13_223503_create_questions_table.php
@@ -16,7 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('course_id');
-            $table->string('question'); 
+            $table->string('name'); 
             $table->timestamps();
         });
     }
