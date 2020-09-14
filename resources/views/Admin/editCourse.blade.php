52d4c0c2eb90c24c5a17cd8ff41ae920fd27464d
Kenneth <kyleabs20@gmail.com>
2020-09-14 01:20:34 -0700

    questions func

--- resources/views/Admin/editCourse.blade.php
+++ resources/views/Admin/editCourse.blade.php
@@ -291,7 +291,7 @@ h2 {
               </a>
           </div>
           <!-- Modal -->
-          <form action="{{route('question.create', $course->id)}}">
+          <form action="{{route('question.create', $course->id)}}" method="POST">
             <div class="modal fade bd-example-modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                   <div class="modal-content">
