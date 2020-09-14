464a46dbe784b350b9c8a73e386db60a9aa7aac3
Kenneth <kyleabs20@gmail.com>
2020-09-14 01:09:15 -0700

    questions func

--- resources/views/Admin/editCourse.blade.php
+++ resources/views/Admin/editCourse.blade.php
@@ -315,8 +315,7 @@ h2 {
                       </div>
                     </div>
                     <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                      <button type="button" class="btn btn-primary">Save changes</button>
+                      <button type="submit" class="btn btn-primary">Save changes</button>
                     </div>
                   </div>
               </div>
