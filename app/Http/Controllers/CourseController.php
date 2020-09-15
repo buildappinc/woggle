f30101f731ee8d419cbe5c73bdd3616d2fa1e98c
Kenneth <kyleabs20@gmail.com>
2020-09-15 00:46:16 -0700

    table modified

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -47,6 +47,7 @@ class CourseController extends Controller
             $progress->save();
         }
 
+        dd($check_section_id->status);
         $next = Topic::where('id', '>', $topic->id)->min("id");
         if ($check_section_id->status == false) {
             $check_section_id->status = true
