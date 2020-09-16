023de0ef749cf0b1fe8a50595fdec024d8c9eb79
Kenneth <kyleabs20@gmail.com>
2020-09-16 15:28:59 -0700

    work

--- resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
+++ resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div>
-       <div class="grid grid-cols-3 gap-4">
+       <div class="grid grid-cols-5 gap-4">
            <div class="col-span-1">dasdadasda</div>
-           <div class="col-span-2">sdasdadada</div>
+           <div class="col-span-4">sdasdadada</div>
        </div>
     </div>
 @endsection
