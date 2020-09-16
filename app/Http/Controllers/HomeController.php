604ea026fa99f8110040400cbc5a5f54ad7ac410
Kenneth <kyleabs20@gmail.com>
2020-09-15 21:38:06 -0700

    please work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,7 +90,7 @@ class HomeController extends Controller
 
     public function QuizSection(Course $course){
         if (Request::ajax()) {
-            return "this is working"
+            return "this is working";
         }
     }
 
