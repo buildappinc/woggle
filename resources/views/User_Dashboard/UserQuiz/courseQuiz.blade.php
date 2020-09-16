ed038b4e419118a696ca1414d06a09739c8b3b96
Kenneth <kyleabs20@gmail.com>
2020-09-16 11:04:37 -0700

    work

--- resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
+++ resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
@@ -3,5 +3,16 @@
 @section('content')
     <div>
         where the party dey today rock star erh  {{$course->name}}
+
+        <div>
+            @foreach ($course->questions as $item)
+                <li>
+                    {{$item->name}}
+                    @foreach ($item->answers as $items)
+                        <li>{{$items->option}}</li>
+                    @endforeach
+                </li>
+            @endforeach
+        </div>
     </div>
 @endsection
