bd207029e33dac90eafe096ae38924515eea2178
Kenneth <kyleabs20@gmail.com>
2020-09-16 15:30:59 -0700

    work

--- resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
+++ resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div>
        <div class="grid grid-cols-5 gap-4">
-           <div class="col-span-1">dasdadasda</div>
+           <div class="col-span-1 bg-red-400 w-full h-full p-0 m-0">dasdadasda</div>
            <div class="col-span-4">sdasdadada</div>
        </div>
     </div>
