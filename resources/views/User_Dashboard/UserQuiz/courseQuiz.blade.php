b4b80cd07f2e3e2cd6842f2f2841869e74138b26
Kenneth <kyleabs20@gmail.com>
2020-09-17 17:22:12 -0700

    restructuring everything

--- resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
+++ resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
@@ -3,8 +3,18 @@
 @section('content')
     <div>
        <div class="grid grid-cols-5 gap-4">
-           <div class="col-span-1 bg-red-400 w-full h-full p-0 m-0">dasdadasda</div>
-           <div class="col-span-4">sdasdadada</div>
+           <div class="col-span-1 bg-red-400 w-full h-full hidden lg:block xl:block">dasdadasda</div>
+           <div class="col-span-4">
+               <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 mx-4">
+                   <div>dnkadnjasd</div>
+                   <div>sadadadada</div>
+                   <div>adsdadsdad</div>
+               </div>
+               {{-- questions --}}
+               <div>
+
+               </div>
+           </div>
        </div>
     </div>
 @endsection
