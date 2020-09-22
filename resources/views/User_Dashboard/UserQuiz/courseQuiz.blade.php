8c66661812e95aa3a0b61fc37e988292639f580d
Kenneth <kyleabs20@gmail.com>
2020-09-22 16:56:31 -0700

    back to work

--- resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
+++ resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
@@ -61,6 +61,7 @@
     </div>
     
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script type="text/javascript">
     console.log("pleasse")
 	var interval;
