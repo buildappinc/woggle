7e9a4093011bd4b21707cbf4a1b17fbe67301e6b
Kenneth <kyleabs20@gmail.com>
2020-09-22 16:31:25 -0700

    back to work

--- resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
+++ resources/views/User_Dashboard/UserQuiz/courseQuiz.blade.php
@@ -25,7 +25,7 @@
                    </div>
                    <div class="rounded shadow-md p-5" style="background: #f2c5c4">
                        <div>CountDown</div>
-                       <div>T - 3:00:00</div>
+                       <div>T - <span class="js-timeout">2:00</span></div>
                    </div>
                    <div class="rounded shadow-md p-5" style="background: #f2d99f">
                        <div>Questions</div>
@@ -60,3 +60,30 @@
        </div>
     </div>
 @endsection
+
+<script type="text/javascript">
+	var interval;
+	function countdown() {
+	  clearInterval(interval);
+	  interval = setInterval( function() {
+	      var timer = $('.js-timeout').html();
+	      timer = timer.split(':');
+	      var minutes = timer[0];
+	      var seconds = timer[1];
+	      seconds -= 1;
+	      if (minutes < 0) return;
+	      else if (seconds < 0 && minutes != 0) {
+	          minutes -= 1;
+	          seconds = 59;
+	      }
+	      else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+	      $('.js-timeout').html(minutes + ':' + seconds);
+
+	      if (minutes == 0 && seconds == 0) { clearInterval(interval); alert('time UP'); }
+	  }, 1000);
+	}
+
+	$('.js-timeout').text("02:00");
+	countdown();
+</script>
