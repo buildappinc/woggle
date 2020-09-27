50d932d7d4641be6da7be3aeb5d038cc67acdceb
Kenneth <kyleabs20@gmail.com>
2020-09-26 22:39:28 -0700

    sup

--- resources/views/User_Dashboard/edit.blade.php
+++ resources/views/User_Dashboard/edit.blade.php
@@ -107,10 +107,9 @@
 
     {{-- button  --}}
     
-    <div class="mb-4 mt-16 ml-0 mr-0">
+    <div class="mb-0 mt-16 ml-0 mr-0">
         <div class="flex justify-center">
-            <input type="hidden" class="delete_value" value="{{Auth::user()->id}}">
-            <button type="submit" class="deleteSection" style="background: linear-gradient(190.3deg, #666666 0%, #151522 100%); width: 37%" class="btn btn-primary h-12">
+            <button type="submit" style="background: linear-gradient(190.3deg, #666666 0%, #151522 100%); width: 37%" class="btn btn-primary h-12">
                 <div class="flex flex-row justify-center">
                     <div class="mx-2">
                         <img src="{{asset('/images/reset.png')}}" alt="">
@@ -163,7 +162,7 @@
               e.preventDefault()
 
               var delete_val = $(this).closest("div").find(".delete_value").val();
-              console.log(delete_val)
+              // console.log(delete_val)
 
               swal({
               title: "Delete!!!",
