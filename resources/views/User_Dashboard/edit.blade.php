a4048b8d3c686a5e51cbfddb74748f11b3d85212
Kenneth <kyleabs20@gmail.com>
2020-09-26 22:37:05 -0700

    sup

--- resources/views/User_Dashboard/edit.blade.php
+++ resources/views/User_Dashboard/edit.blade.php
@@ -107,7 +107,7 @@
 
     {{-- button  --}}
     
-    <div class="mb-0 mt-16 ml-0 mr-0">
+    <div class="mb-4 mt-16 ml-0 mr-0">
         <div class="flex justify-center">
             <input type="hidden" class="delete_value" value="{{Auth::user()->id}}">
             <button type="submit" class="deleteSection" style="background: linear-gradient(190.3deg, #666666 0%, #151522 100%); width: 37%" class="btn btn-primary h-12">
