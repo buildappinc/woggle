b66de87f90128c79b25de2125b3ef1fe59ee54bc
Kenneth <kyleabs20@gmail.com>
2020-09-30 00:31:14 -0700

    this way

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -69,7 +69,7 @@ class CourseController extends Controller
             $user_payment->update([
                 'status_delete' => $user_payment->status_delete
             ]);
-            dd($authUser->courses());
+            dd($authUser->courses);
             $authUser->courses()->update([
                 'status' => $user_course->status
             ]);
@@ -81,7 +81,7 @@ class CourseController extends Controller
             $user_payment->update([
                 'status_delete' => $user_payment->status_delete
             ]);
-            dd($authUser->courses());
+            dd($authUser->courses);
             $authUser->courses()->update([
                 'status' => $user_course->status
             ]);
