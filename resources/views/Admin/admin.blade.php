95dac7a8669b48d5b2c12e7d2f26f0c095a2167c
Kenneth <kyleabs20@gmail.com>
2020-10-07 16:29:01 -0700

    notification

--- resources/views/Admin/admin.blade.php
+++ resources/views/Admin/admin.blade.php
@@ -115,13 +115,18 @@
                         </a>  
                     </div>
                     @foreach ($courses as $item)
-                        <div class="w-full p-4 flex flex-col justify-around col-gap-6 ">
+                        {{-- <div class="w-full p-4 flex flex-col justify-around col-gap-6 ">
                             <a class="text-black" href="/admin/courses/{{$item->id}}/edit">
                                 <div class="w-11/12 h-16 bg-gray-200 rounded-lg flex flex-row justify-between px-4 p-4 hover:shadow-lg">
                                     <div>{{$item->name}}</div>
                                     <div>{{$item->price}}</div>
                                 </div>
                             </a>
+                        </div> --}}
+                        <div class="p-4 flex md:flex-wrap lg:flex-wrap xl:flex-wrap ">
+                            <div class="w-2/4 bg-red-400">
+                                dsakldmsadd
+                            </div>
                         </div>
                     @endforeach
                     
--- resources/views/Admin/app.blade.php
+++ resources/views/Admin/app.blade.php
@@ -258,8 +258,8 @@
    
         <main class="pt-4">
              <!-- Modal -->
-             <div class="modal fade modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+             <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                      <h5 class="modal-title" id="exampleModalLongTitle">Notifications</h5>
