3bb706e04beb4bd864fbb93585fd2e419db56f62
Kenneth <kyleabs20@gmail.com>
2020-10-07 16:10:43 -0700

    notification

--- resources/views/Admin/app.blade.php
+++ resources/views/Admin/app.blade.php
@@ -258,21 +258,18 @@
    
         <main class="pt-4">
              <!-- Modal -->
-             <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
+             <div class="modal fade modal-lg" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                     <h5 class="modal-title" id="exampleModalLongTitle">Notifications</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                     </div>
                     <div class="modal-body">
-                    ...
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                        ...
+                        leave all your notifications here 
                     </div>
                 </div>
                 </div>
