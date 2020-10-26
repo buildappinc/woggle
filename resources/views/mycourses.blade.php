44fd377fb2607d33a72c25b2b525da794da697e7
Kenneth <kyleabs20@gmail.com>
2020-10-26 15:31:41 -0700

    mycourses

--- resources/views/mycourses.blade.php
+++ resources/views/mycourses.blade.php
@@ -87,7 +87,7 @@
 
                     @if (Auth::user()->courses->count() <= 0 )
                         <div class="md:mt-8 lg:mt-8 xl:mt-8">
-                            <div class="text-center flex justify-center items-center text-md h-40 w-full rounded" style="border:1px dashed #000000">
+                            <div class="text-center flex justify-center items-center h-32 w-11/12 rounded hello" style="border:1px dashed #000000">
                                 Opps No Subscribed Course!!! <a href="/study" style="color: #f7b728">Check Out Available Courses</a> 
                             </div>
                         </div>
