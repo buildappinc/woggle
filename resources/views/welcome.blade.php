7ff9001374ca3c8acc96535d7f24c6f546551549
Kenneth <kyleabs20@gmail.com>
2020-11-14 20:24:07 -0800

    dealing with front facing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -272,7 +272,7 @@
             <div class="homeImage animate__fadeInLeft">
                 <div class="z-0"></div>
                 <div class="font hidden md:block lg:block xl:block text-3xl z-10 py-64" style="height: 70%">
-                    <div class="text-xl uppercase text-black flex justify-center items-center whitespace-normal" style="color: #000"><span style="color: #f9b629" class="mx-3"> the </span> <span style="color: #fff" class="mx-3">best online</span> <span style="color: #f9b629" class="mx-3"> education </span> in the world</div>
+                    <div class="text-2xl uppercase text-black flex justify-center items-center whitespace-normal" style="color: #000"><span style="color: #f9b629" class="mx-3"> the </span> <span style="color: #fff">best online</span> <span style="color: #f9b629" class="mx-3"> education </span> in the world</div>
                     <div class="text-5xl uppercase text-black flex justify-center items-center whitespace-normal" style="color: #000">one <span style="color: #000" class="mx-3"> steps </span> ahead this season</div>
                 </div>
                 <div class="wavy"></div>
