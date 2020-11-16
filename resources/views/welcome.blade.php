09551216cbc58625bddf02df5fcc5b0c23dfcedf
Kenneth <kyleabs20@gmail.com>
2020-11-15 20:09:24 -0800

    images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -190,7 +190,7 @@
     .subimage {
         position: relative;
         background-attachment: fixed;
-        background-image: url('{{asset('images/sub.jpg')}}');
+        background-image: url('{{asset('images/subs.jpg')}}');
         background-size: cover;
         z-index: 1;
     }
