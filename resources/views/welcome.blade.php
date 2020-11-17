4bd3d9bcb15b36fd1bfc3e20cc4f813f7cf29b85
Kenneth <kyleabs20@gmail.com>
2020-11-16 20:56:46 -0800

    resolving errors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -320,8 +320,8 @@
                 </div>
         </nav>
         {{-- desktop view --}}
-        <div class="homeImage hidden lg:block xl:block animate__fadeInLeft">
-            <div class="colortext font text-3xl py-64 w-full" style="height: 70%">
+        <div class="homeImage animate__fadeInLeft">
+            <div class="colortext hidden md:block lg:block xl:block font text-3xl py-64 w-full" style="height: 70%">
                 <div data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine" data-aos-duration="800" class="text-xl uppercase text-black flex justify-center items-center lg:mt-12 xl:mt-12" style="color: #fff">the best online education <span style="color: #000" class="mx-3">in the world</span></div>
                 <div data-aos="fade-up" data-aos-offset="300" data-aos-easing="ease-in-sine" data-aos-duration="800" class="text-5xl uppercase text-black flex justify-center items-center font-bold mt-3" style="color: #000">one <span style="color: #000" class="mx-3"> steps </span> ahead this season</div>
                 <div class="flex justify-center mt-8">
