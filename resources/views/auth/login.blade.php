b67ad9346132e01fe4778677689c772ce7fa8ed0
Kenneth <kyleabs20@gmail.com>
2020-11-17 14:51:37 -0800

    resolving errors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,6 +23,12 @@
         left: 32vw;
     }
 
+    .font{
+        font-family: 'Baloo Tamma 2', cursive;
+        font-family: 'Raleway', sans-serif;
+        font-family: 'Recursive', sans-serif;
+    }
+
     .btn-primary{
         background: linear-gradient(184.38deg, #779FDD 7.68%, #406AB4 106.58%);
         border-radius: 5px;
@@ -37,13 +43,13 @@
 
 <div class="body">
     <div class="box flex justify-center">
-        <div class="text-white text-3xl italic mt-20 font-extrabold">Welcome to Woggle Africa</div>
+        <div class="font text-white text-3xl italic mt-20 font-extrabold">Welcome to Woggle Africa</div>
     </div>
 
     <div class="box-2 rounded-md shadow-sm p-16">
         {{-- sign in name --}}
         <div class="flex justify-center">
-            <div class="text-2xl text-black font-bold">
+            <div class="font text-2xl text-black font-bold">
                 Sign In
             </div>
         </div>
@@ -55,7 +61,7 @@
                 <div class="row">
                     <div class="col-12">
                             <div class="form-group">
-                                <label for="email">{{ __('E-Mail Address') }}</label>
+                                <label class="font" for="email">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-12 input">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -73,7 +79,7 @@
             <div class="row">
                 <div class="col-12">
                             <div class="form-group">
-                                <label for="password" >{{ __('Password') }}</label>
+                                <label class="font" for="password" >{{ __('Password') }}</label>
 
                                 <div class="col-md-12 input">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -92,7 +98,7 @@
                 {{-- button  --}}
                 <div class="form-group row mb-0 ml-0 mr-0 mt-8">
                     <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary h-12 w-full">
+                        <button type="submit" class="font btn btn-primary h-12 w-full">
                             {{ __('Login') }}
                         </button>
                     </div>
@@ -103,14 +109,14 @@
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                            <label class="form-check-label" for="remember">
+                            <label class="font" class="form-check-label" for="remember">
                                 {{ __('Remember Me') }}
                             </label>
                         </div>
                     </div>
                 </div> 
                 {{-- sign up --}}
-                <div class="mt-32 flex justify-center">
+                <div class="font mt-32 flex justify-center">
                     <div>Don't have an account? <a href="#">Sign Up</a></div>
                 </div>
 
