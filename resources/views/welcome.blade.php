f205b657fb8e7f379465975376f481b06324e4a3
Kenneth <kyleabs20@gmail.com>
2020-11-17 09:57:01 -0800

    resolving errors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -181,7 +181,7 @@
             background: #fb0040;
             background-attachment: fixed;
             background-image: url('{{asset('images/boy-in-yellow-crew-neck-t-shirt-while-holding-tablet-4144103.jpg')}}');
-            /* background-size: cover; */
+            background-size: cover;
             z-index: 1
         }
     }
@@ -193,9 +193,9 @@
             max-width: 100%;
             height: 100vh;
             background: #fb0040;
-            /* background-attachment: fixed;
-            background-image: url('{{asset('images/boy-in-yellow-crew-neck-t-shirt-while-holding-tablet-4144103.jpg')}}'); */
-            /* background-size: cover; */
+            background-attachment: fixed;
+            background-image: url('{{asset('images/boy-in-yellow-crew-neck-t-shirt-while-holding-tablet-4144103.jpg')}}');
+            background-size: cover;
             z-index: 1
         }
     }
