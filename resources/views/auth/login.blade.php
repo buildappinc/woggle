02370483ab7a7e224311f49a6a9c2c0df1b07fad
Kenneth <kyleabs20@gmail.com>
2020-11-19 14:40:22 -0800

    second section of the quiz

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,6 +23,17 @@
         left: 32vw;
     }
 
+    @media screen and (max-width: 425px){
+        .box-2{
+            width: 40vw;
+            height: 75vh;  
+            background: #F5F5F5;
+            position: absolute;
+            top: 22vh;
+            left: 32vw;
+        }
+    }
+
     .font{
         font-family: 'Baloo Tamma 2', cursive;
         font-family: 'Raleway', sans-serif;
@@ -41,9 +52,93 @@
 
 @section('content')
 
+{{-- normal section --}}
 <div class="body hidden md:block lg:block xl:block">
     <div class="box flex justify-center">
         <div class="font text-white text-3xl italic mt-20 font-extrabold">Welcome to Woggle Africa</div>
+    </div>
+
+    <div class="box-2 rounded-md shadow-sm p-16">
+        {{-- sign in name --}}
+        <div class="flex justify-center">
+            <div class="font text-2xl text-black font-bold">
+                Sign In
+            </div>
+        </div>
+        {{-- login form --}}
+        <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="mt-16">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label class="font" for="email">{{ __('E-Mail Address') }}</label>
+
+                                <div class="col-md-12 input">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                    
+                            </div>
+                        </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label class="font" for="password" >{{ __('Password') }}</label>
+
+                            <div class="col-md-12 input">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                {{-- button  --}}
+                <div class="form-group row mb-0 ml-0 mr-0 mt-8">
+                    <div class="col-md-12">
+                        <button type="submit" class="font btn btn-primary h-12 w-full">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+                </div>
+                {{-- remember me  --}}
+                <div class="form-group">
+                    <div class="col-md-6">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                            <label class="font" class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                        </div>
+                    </div>
+                </div> 
+                {{-- sign up --}}
+                <div class="font mt-32 flex justify-center">
+                    <div>Don't have an account? <a href="#">Sign Up</a></div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+{{-- mobile section --}}
+<div class="body md:hidden lg:hidden xl:hidden">
+    <div class="box flex justify-center">
+        <div class="font text-white text-xl italic mt-20 font-extrabold">Welcome to Woggle Africa</div>
     </div>
 
     <div class="box-2 rounded-md shadow-sm p-16">
