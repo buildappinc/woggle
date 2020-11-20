8ec5cd725bf51dcfcd5710ddf490eb5f3f10cddd
Kenneth <kyleabs20@gmail.com>
2020-11-19 17:32:03 -0800

    second section of the quiz

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -179,7 +179,7 @@
                                 <label class="font block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
                                     Phone Number
                                 </label>
-                                <input name="phone" class="@error('phone') is-invalid @enderror appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0234*****">
+                                <input name="phone" value="{{Auth::user()->phone}}" class="@error('phone') is-invalid @enderror appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0234*****">
                                 @error('phone')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
