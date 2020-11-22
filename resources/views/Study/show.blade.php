da3deff43bd2726dc984f70e826bac16daaf29d5
Kenneth <kyleabs20@gmail.com>
2020-11-21 18:55:41 -0800

    style sheet

--- resources/views/Study/show.blade.php
+++ resources/views/Study/show.blade.php
@@ -18,10 +18,7 @@
 @section('title', 'course')
 
 @section('content')
-
-
-<div class="container-fluid">
-
+<div class="container-fluid hidden md-block lg:block xl:block">
     <div class="px-32 py-10">
         <div class="grid grid-cols-4 gap-4">
             <div class="col-span-3 ... ">
@@ -42,10 +39,6 @@
                                                   <p style="text-align: center; padding-top: 6%; text-transform: uppercase" class="font">Enroll now</p> 
                                             </div>
                                         </a>
-                                        {{-- <form action="{{route('course.get', ['course'=>$course->id])}}" method="post">
-                                            @csrf
-                                           <button type="submit" class="button mt-3" style="background-color: #f7b728">Enroll now</button>
-                                        </form> --}}
                                     </div>
                                 </div>
                                 <div class="col-span-3 ...">
@@ -110,7 +103,75 @@
                 </div>
             </div>
         </div>
-    </div>
-    
+    </div>  
+</div>
+
+{{-- mobile sectin  --}}
+<div class="container-fluid  md-hidden lg:hidden xl:hidden">
+    <div class="px-8 py-10">
+        <div class="grid grid-cols-1">
+            <div class="grid grid-rows-3 grid-flow-col gap-4">
+                <div class="row-span-1 col-span-2 ...">
+                    {{-- top-left section --}}
+                    <div class="shadow-md rounded-md p-4 animated fadeInLeft slower-4s">
+                        {{-- inner top left section  --}}
+                        <div class="grid grid-cols-1">
+                            <div>
+                                <div class="h-48">
+                                    <img class="rounded-lg h-48 max-h-full shadow-inner object-cover" src="{{asset('images/courses/'.$course->image)}}" alt="">
+                                </div>
+                                {{-- {{route('course.get', ['course'=>$course->id])}} --}}
+                                <div class="mt-3">
+                                    <a href="{{ route('payment.index', ['course'=>$course->id])}}" style="text-decoration: none; color: #000">
+                                        <div style="background-color: #f7b728; height: 40px; width: 100%; border-radius: 3px;">
+                                                <p style="text-align: center; padding-top: 6%; text-transform: uppercase" class="font">Enroll now</p> 
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <h2 class="font">
+                                        {{$course->name}}
+                                    </h2>
+                                    <div> <p style="line-height: 30px;" class="tect_font text-justify">{{$course->description}}</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row-span-2 col-span-2 ...">
+                    {{-- buttom right section --}}
+                    <div class="shadow-md rounded-md p-4 animated fadeInUp">
+                        <div class="flex justify-between">
+                            <div class="font">Topics</div>
+                            <div class="font">Practical exercise</div>
+                        </div>
+
+                        <div class="mt-3">
+                            @foreach ($course->topics as $topic)
+                                <div>
+                                    <div class="font pt-2 pl-3 text-base pb-2" style="background: linear-gradient(183.72deg, #666666 0%, #151522 100%); border-radius: 5px; color: #fff">
+                                    Topic: {{$topic->name}}
+                                    </div>
+                                    <div class="mt-2">
+                                    @foreach ($topic->sections as $section)
+                                        <div class="p-1" style="background-color: #f7f7f7">
+                                        <div class="flex justify-between">
+                                            <div class="text_font pl-3">Section - {{$section->header}}</div>
+                                            <div class="font pr-3">10 mins</div>
+                                        </div>
+                                        <hr>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>   
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
 </div>
 @endsection
